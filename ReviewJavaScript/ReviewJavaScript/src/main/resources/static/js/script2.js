7295aec1a0dccafd54ce35a9e6d6e88eefb8a885
Pham Quoc Duy <phamquocduy1110@gmail.com>
2021-09-23 04:50:56 +0700

    Variable statements, notes, and basic calculations

---
+++ ReviewJavaScript/ReviewJavaScript/src/main/resources/static/js/script2.js
@@ -0,0 +1,20 @@
+var biencuatoi = "Xin chào các bạn!";
+alert(biencuatoi);
+
+var a = 5;
+var b = '6';
+var c = a + b;
+
+alert('Đây là alert 2: ' + c)
+
+var a = 5;
+var b = '6';
+a++;
+
+alert('Đây là alert 3: ' + a)
+
+var a = 5;
+var b = '6';
+a += 5;
+
+alert('Đây là alert 4: ' + a)
